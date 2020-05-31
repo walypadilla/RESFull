652a5ef70401787826609bcefd21728d9b48f6c1
Anartz Mugika Ledo <mugan86@gmail.com>
2020-05-31 21:57:19 +0200

    Rename with correct values

--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -1,6 +1,6 @@
 const { sign } = require('jsonwebtoken');
-const { JTW_SECRET } = require('../config/config');
+const { JWT_SECRET } = require('../config/config');
 
 module.exports.generateToken = function (user) {
-	return sign({ user }, JTW_SECRET, { expiresIn: '8h' });
+	return sign({ user }, JWT_SECRET, { expiresIn: '8h' });
 };
